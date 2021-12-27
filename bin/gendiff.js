0c7a92c192e7010f23cb79d091e755466969edbe
MaximKalinchuk <kalinchuk.maxim@mail.ru>
2021-12-27 20:41:39 +0300

    updated bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-v, --version', 'output the current version')
+
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1> <filepath2>')
 program.parse();
