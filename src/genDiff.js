ada33f66f89e07232977aaaf4740240bde4644dd
MaximKalinchuk <kalinchuk.maxim@mail.ru>
2022-02-03 11:41:08 +0300

    updated genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -17,17 +17,10 @@ const genDiff = (filepath1, filepath2, formatName) => {
   const content1 = parse(filepath1);
   const content2 = parse(filepath2);
 
-  // const keys = getSortedKeys(content1, content2);
-
   const buildTree = (value1, value2) => {
     const keys = getSortedKeys(value1, value2);
 
     const comparisonResult = keys.map((key) => {
-      // 1. 1 объекте ключа нет - add
-      // 2. 2 объекте ключа нет - deleted
-      // 3. 2 значения === объект, строим деверо
-      // 4. 1 об === 2 об значения не равны - changed
-      // 5. 1 об === 2 об значения равны - unchanged
       if (!_.has(value1, key)) {
         return { key, value2: value2[key], type: 'add' };
       }
@@ -50,10 +43,10 @@ const genDiff = (filepath1, filepath2, formatName) => {
   };
 
   const tree = buildTree(content1, content2);
-  // console.log(tree);
-  const resultTree = selectFormat(tree, formatName);
-  console.log(resultTree);
-  return resultTree;
+
+  const normalizedTree = selectFormat(tree, formatName);
+  console.log(normalizedTree);
+  return normalizedTree;
 };
 
 export default genDiff;
