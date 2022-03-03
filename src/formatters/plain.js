3bdf406b670446aaa817b05f6271a987b7c67546
MaximKalinchuk <kalinchuk.maxim@mail.ru>
2022-03-03 22:19:32 +0300

    fix stylish.js and plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,20 +11,22 @@ const stringify = (value) => {
   return value;
 };
 
-const formatPlain = (objDiff, path = []) => {
-  const formatterByNodeTypes = {
-    add: (value1, value2, children, filepath, key) => (`Property '${[...path, key].join('.')}' was added with value: ${stringify(value2)}`),
-    del: (value1, value2, children, filepath, key) => (`Property '${[...path, key].join('.')}' was removed`),
-    object: (value1, value2, children, filepath, key) => (formatPlain(children, [...path, key])),
-    changed: (value1, value2, children, filepath, key) => (`Property '${[...path, key].join('.')}' was updated. From ${stringify(value1)} to ${stringify(value2)}`),
+const formatterByNodeTypes = {
+  add: (children, filepath, key, iter, value1, value2) => (`Property '${[...filepath, key].join('.')}' was added with value: ${stringify(value2)}`),
+  del: (children, filepath, key) => (`Property '${[...filepath, key].join('.')}' was removed`),
+  object: (children, filepath, key, iter) => (iter(children, [...filepath, key])),
+  changed: (children, filepath, key, iter, value1, value2) => (`Property '${[...filepath, key].join('.')}' was updated. From ${stringify(value1)} to ${stringify(value2)}`),
+  unchanged: () => ([]),
+};
+
+const formatPlain = (objDiff) => {
+  const iter = (obj, path = []) => {
+    const resultItems = obj.map(({
+      key, value1, value2, type, children,
+    }) => formatterByNodeTypes[type](children, path, key, iter, value1, value2));
+    return resultItems.flat().join('\n');
   };
-
-  const correctItems = objDiff.filter(({ type }) => type !== 'unchanged');
-  const resultItems = correctItems.map(({
-    key, value1, value2, type, children,
-  }) => formatterByNodeTypes[type](value1, value2, children, path, key));
-
-  return resultItems.join('\n');
+  return iter(objDiff);
 };
 
 export default formatPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -22,26 +22,30 @@ const stringify = (stringifiable, depth = 0) => {
   return iter(stringifiable, depth);
 };
 
+const formatterByNodeTypes = {
+  add: (value1, value2, children, indent, key, identDepth) => (`${indent}  + ${key}: ${stringify(value2, identDepth + 1)}`),
+  del: (value1, value2, children, indent, key, identDepth) => (`${indent}  - ${key}: ${stringify(value1, identDepth + 1)}`),
+  object: (value1, value2, children, indent, key, identDepth, iter) => (`${indent}    ${key}: ${iter(children, identDepth + 1)}`),
+  changed: (value1, value2, children, indent, key, identDepth) => ([
+    `${indent}  - ${key}: ${stringify(value1, identDepth + 1)}`,
+    `${indent}  + ${key}: ${stringify(value2, identDepth + 1)}`,
+  ]),
+  unchanged: (value1, value2, children, indent, key, identDepth) => (`${indent}    ${key}: ${stringify(value1, identDepth)}`),
+};
+
 const formatStylish = (objDiff) => {
   const iter = (obj, depth = 0) => {
-    const formatterByNodeTypes = {
-      add: (value1, value2, children, indent, key, identDepth) => (`${indent}  + ${key}: ${stringify(value2, identDepth + 1)}`),
-      del: (value1, value2, children, indent, key, identDepth) => (`${indent}  - ${key}: ${stringify(value1, identDepth + 1)}`),
-      object: (value1, value2, children, indent, key, identDepth) => (`${indent}    ${key}: ${iter(children, identDepth + 1)}`),
-      changed: (value1, value2, children, indent, key, identDepth) => (`${indent}  - ${key}: ${stringify(value1, identDepth + 1)}\n${indent}  + ${key}: ${stringify(value2, identDepth + 1)}`),
-      unchanged: (value1, value2, children, indent, key, identDepth) => (`${indent}    ${key}: ${stringify(value1, identDepth)}`),
-    };
     const indent = '    '.repeat(depth);
 
     const resultItems = obj.map(({
       key, value1, value2, type, children,
-    }) => formatterByNodeTypes[type](value1, value2, children, indent, key, depth));
+    }) => formatterByNodeTypes[type](value1, value2, children, indent, key, depth, iter));
 
     return [
       '{',
       ...resultItems,
       `${indent}}`,
-    ].join('\n');
+    ].flat().join('\n');
   };
   return iter(objDiff, 0);
 };
